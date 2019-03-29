b106ad912d04c3b6b2d63cbf6776a5c54eb162c4
huazi <huazifly@gmail.com>
2019-03-29 21:42:00 +0800

    update

--- include/utils/file_utils.h
+++ include/utils/file_utils.h
@@ -15,6 +15,8 @@
 #include <unistd.h>
 #include <fstream>
 #include <iostream>
+#include <vector>
+#include <string>
 #define SMALL_READ_DATA_SIZE  512
 #define READ_DATA_SIZE  4096000 //4M
 #define MD5_SIZE        32
@@ -121,7 +123,7 @@ public:
 		md5_ret = md5_str;
 		return true;
 	}*/
-    static bool GetDirFiles(const char * dir, vector<string> & file_names) {
+    static bool GetDirFiles(const char * dir, std::vector<std::string> & file_names) {
         DIR * directory_pointer;
         struct dirent * entry;
         if((directory_pointer = opendir(dir)) == NULL) {
@@ -133,8 +135,8 @@ public:
         }
         return true;
     }
-    static bool ReadConfFileContext(const char * file, string & file_context, const char comment_char = '#') {
-        ifstream in(file);
+    static bool ReadConfFileContext(const char * file, std::string & file_context, const char comment_char = '#') {
+        std::ifstream in(file);
         if(!in) {
             return false;
         }
@@ -147,8 +149,8 @@ public:
         in.close();
         return true;
     }
-    static bool ReadConfFile2Map(const char * file, vector<string> & res, const char comment_char = '#') {
-        ifstream in(file);
+    static bool ReadConfFile2Map(const char * file, std::vector<std::string> & res, const char comment_char = '#') {
+        std::ifstream in(file);
         if(!in) {
             return false;
         }
@@ -161,6 +163,38 @@ public:
         in.close();
         return true;
     }
+    static std::string GetFileDir(const std::string & file_path) { //获取文件的路径
+        int l = file_path.size();
+        int i = l - 1;
+        for(; i >= 0; i--) {
+            char c = file_path[i];
+            if(c == '/') break;
+        }
+        if(i > 0) {
+            return file_path.substr(0, i + 1);
+        }
+        return "";
+    }
 
+    static int MkPath(std::string s,mode_t mode=0755) {
+        size_t pre=0,pos;
+        std::string dir;
+        int mdret;
+
+        if(s[s.size()-1]!='/'){
+            // force trailing / so we can handle everything in loop
+            s+='/';
+        }
+
+        while((pos=s.find_first_of('/',pre))!=std::string::npos){
+            dir=s.substr(0,pos++);
+            pre=pos;
+            if(dir.size()==0) continue; // if leading / first time is 0 length
+            if((mdret=::mkdir(dir.c_str(),mode)) && errno!=EEXIST){
+                return mdret;
+            }
+        }
+        return mdret;
+    }
 };
 #endif
